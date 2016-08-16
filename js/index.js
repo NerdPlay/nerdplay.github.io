c62009fdffdbf39f061e8edf2bd762f9b83aae66
Danilo Rodrigues <danilo@danilo.ws>
2016-08-16 02:34:02 -0300

    New Upadate selecionar
    number

--- js/index.js
+++ js/index.js
@@ -1,3 +1,36 @@
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length,c.length);
+        }
+    }
+    return "";
+}
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+$(document).ready(function() {
+  var code = getParameterByName('number');
+  console.log(code);
+});
 (function(window, undefined) {
 
 'use strict';
@@ -690,7 +723,13 @@ var AudioPlayer = (function() {
 
     audio.pause();
     apActive = false;
-    index = 0;
+    if(code==null) {
+      index = 0;
+    }
+    else {
+       index = code;
+    }
+    
 
     playBtn.classList.remove('is-playing');
     playSvgPath.setAttribute('d', playSvg.getAttribute('data-play'));
@@ -801,7 +840,8 @@ window.AP = AudioPlayer;
 })(window);
 
 //var https = 'http://jovemnerd.com.br/wp-json/jovemnerd/v1/nerdcasts';
-var https = 'http://192.168.1.80/nerdplay/nerdcasts.json';
+//var https = 'http://nerdplay-ga.umbler.net/json.php';
+//var https = 'http://192.168.1.80/nerdplay/api/json.php';
 //var https = 'https://nerdplay.github.io/nerdcasts.json';
 var playerlists = function () {
         $.ajax({
